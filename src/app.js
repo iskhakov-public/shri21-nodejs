91c47ca3cdebefa40954d6401a3ed1bbe55f1dd2
Наиль Исхаков <nail@iskhakov.me>
2021-10-01 23:45:29 +0500

    error handling on /merge added

--- src/app.js
+++ src/app.js
@@ -112,24 +112,31 @@ app.get('/merge', async (req, res) => {
   //   frontObj = {path: 'assets/S2YnSak84nGPGwosjldT-.jpg', mimeType: "image/jpg"}
   //   backObj = {path: 'assets/sb2DWcibRCjS89efEYL2i.jpg'}
 
-  const frontStr = createReadStream(
-    path.resolve(__dirname, '..', frontObj.path)
-  );
-  const backStr = createReadStream(path.resolve(__dirname, '..', backObj.path));
+  try {
+    const frontStr = createReadStream(
+      path.resolve(__dirname, '..', frontObj.path)
+    );
+    const backStr = createReadStream(
+      path.resolve(__dirname, '..', backObj.path)
+    );
 
-  res.setHeader('Content-type', frontObj.mimeType);
-  //   res.setHeader(
-  //     'Content-Disposition',
-  //     `attachment; filename=merged${path.extname(frontObj.path)}`
-  //   );
+    res.setHeader('Content-type', 'image/jpeg');
+    res.setHeader('Content-Disposition', 'attachment; filename=merged.jpg');
+    //   res.setHeader(
+    //     'Content-Disposition',
+    //     `attachment; filename=merged${path.extname(frontObj.path)}`
+    //   );
 
-  const rstream = await replaceBackground(
-    frontStr,
-    backStr,
-    colors,
-    thresholdNum
-  );
-  rstream.pipe(res);
+    const rstream = await replaceBackground(
+      frontStr,
+      backStr,
+      colors,
+      thresholdNum
+    );
+    rstream.pipe(res);
+  } catch (err) {
+    res.status(400).end();
+  }
 });
 
 // DEBUG
